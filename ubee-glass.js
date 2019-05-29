6ffdd4733c838f645519f5afd67bed867924e3bf
Alan Badillo Salas <badillo.soft@hotmail.com>
2019-05-28 22:14:37 -0500

    v1.1 rev0

--- ubee-glass.js
+++ ubee-glass.js
@@ -1,3 +1,36 @@
+function uuid(length = 16, radix = 32) {
+    let token = "";
+    while (token.length < length) {
+        token += Math.random().toString(radix).slice(2);
+    }
+    return token;
+}
+
+function ubee(...components) {
+    // Process each component
+    components.forEach(element => {
+        element.dataset.glass = uuid();
+
+        ubee_event(element);
+        ubee_listen(element);
+
+        element.addEventListener(":update", () => {
+            ubee_attribute(element);
+            ubee_style(element);
+        });
+
+        fire(element, ":update");
+    });
+
+    return components;
+}
+
+function fire(node, channel, detail) {
+    node.dispatchEvent(new CustomEvent(channel, {
+        detail
+    }));
+}
+
 async function load(url) {
     const response = await fetch(url);
 
@@ -37,33 +70,65 @@ async function load(url) {
     }, []);
 
     const components = pairs.reduce((templates, [open, close]) => {
+        const tag = html.substring(open[0], open[0] + open[1]);
         templates.push([
             html.substring(open[0] + open[1], close[0]),
-            (html.substring(open[0], open[0] + open[1]).match(/data-name="([^"]*)"/) || [])[1]
+            (tag.match(/data-name="([^"]*)"/) || [])[1],
+            (tag.match(/data-hidden="([^"]*)"/) || [])[1],
         ]);
         return templates;
-    }, []).map(([template_html, name]) => {
+    }, []).map(([template_html, name, hidden]) => {
         const template = document.createElement("template");
         template.innerHTML = template_html;
         const node = document.importNode(template.content, true);
-        const component = node.firstElementChild;
-        component.dataset.name = name;
-        return component;
+        const div = document.createElement("div");
+        [...node.childNodes].forEach(component => div.appendChild(component));
+        div.dataset.name = name;
+        div.hidden = hidden === "true";
+        // const component = [...node.children];
+        // component.dataset.name = name;
+        ubee(div);
+        return div;
     });
 
-    const div = document.createElement("div");
+    const container = document.createElement("div");
 
-    div.dataset.source = url;
+    container.dataset.source = url;
 
-    components.forEach(component => div.appendChild(component));
+    components.forEach(component => container.appendChild(component));
 
-    return div;
+    return container;
 }
 
-function fire(node, channel, detail) {
-    node.dispatchEvent(new CustomEvent(channel, {
-        detail
-    }));
+function ubee_template(template, ...selectors) {
+    if (typeof template === "string") {
+        const html = template;
+        template = document.createElement("template");
+        template.innerHTML = html;
+    }
+
+    if (selectors.length === 0) {
+        selectors.push(`:nth-child(1)`);
+    }
+
+    // Get components from selectors
+    const components = selectors.map(selector => {
+            const node = document.importNode(template.content, true);
+            return node.querySelectorAll(selector);
+        }).reduce((components, nodes) => {
+            components.push(...nodes);
+            return components;
+        }, []);
+
+    const container = document.createElement("div");
+
+    container.dataset.template = Math.random().toString(32).slice(2);
+
+    components.forEach(component => container.appendChild(component));
+
+    ubee(container);
+
+    return container;
 }
 
 function ubee_parse(key) {
@@ -75,13 +140,19 @@ function ubee_parse(key) {
 
     variant = variant.replace(/-\d+$/, "");
 
-    if (variant.match(/^at-/)) {
-        variant = variant.replace(/^at-/, "@");
-    } else if (variant.match(/^in-/)) {
-        variant = variant.replace(/^in-/, ":");
-    } else if (variant.match(/^of-/)) {
-        variant = variant.replace(/^of-/, "#");
+    // console.log(variant, instance);
+    
+    if (variant.match(/-?at-/)) {
+        variant = variant.replace(/-?at-/g, "@");
     }
+    if (variant.match(/-?in-/)) {
+        variant = variant.replace(/-?in-/g, ":");
+    }
+    if (variant.match(/-?of-/)) {
+        variant = variant.replace(/-?of-/g, "#");
+    }
+    
+    // console.log(variant, instance);
 
     return [type, variant, instance];
 }
@@ -90,19 +161,43 @@ function ubee_event(element) {
     element.querySelectorAll(`*`).forEach(child => {
         for (let [key, value] of Object.entries(child.dataset || {})) {
             let [type, variant] = ubee_parse(key);
+            
             if (type !== "event") {
                 continue;
             }
-            child.addEventListener(variant, e => {
+            
+            const events = JSON.parse(child.dataset.glassEvent || "{}");
+
+            if (variant in events) {
+                continue;
+            }
+
+            const id = uuid();
+
+            events[variant] = id;
+
+            child.dataset.glassEvent = JSON.stringify(events);
+
+            const callback = e => {
                 e = e instanceof CustomEvent ? e.detail : e;
                 new Function(
                     "self",
                     "parent",
                     "event",
                     "fire",
+                    "template",
                     value
-                )(child, element, e, fire);
-            });
+                )(child, element, e, fire, ubee_template);
+                // console.warn("fire", variant, id, child, element, e);
+            };
+
+            window.events = window.events || {};
+
+            window.events[id] = callback;
+
+            // console.warn("event", variant, id, child, element);
+
+            child.addEventListener(variant, callback);
         }
     });
 }
@@ -111,20 +206,47 @@ function ubee_listen(element) {
     element.querySelectorAll(`*`).forEach(child => {
         for (let [key, value] of Object.entries(child.dataset || {})) {
             let [type, variant] = ubee_parse(key);
+            
             if (type !== "listen") {
                 continue;
             }
-            element.addEventListener(variant, e => {
+            
+            const listeners = JSON.parse(child.dataset.glassListen || "{}");
+
+            if (variant in listeners) {
+                continue;
+            }
+
+            const id = uuid();
+
+            listeners[variant] = id;
+
+            child.dataset.glassListen = JSON.stringify(listeners);
+
+            const callback = e => {
                 e = e instanceof CustomEvent ? e.detail : e;
                 new Function(
                     "self",
                     "parent",
                     "event",
                     "fire",
+                    "template",
                     value
-                )(child, element, e, fire);
-            });
+                )(child, element, e, fire, ubee_template);
+            };
+
+            window.listeners = window.listeners || {};
+
+            window.listeners[id] = callback;
+
+            element.addEventListener(variant, callback);
         }
+    });
+}
+
+function ubee_notify(element, channel, detail) {
+    element.querySelectorAll(`*`).forEach(child => {
+        fire(child, channel, detail);
     });
 }
 
@@ -152,49 +274,4 @@ function ubee_style(element) {
             }
         }
     });
-}
-
-function ubee_template(template, ...selectors) {
-    if (typeof template === "string") {
-        const html = template;
-        template = document.createElement("template");
-        template.innerHTML = html;
-    }
-
-    // Get components from selectors
-    const components = selectors.length === 0 ?
-        [document.importNode(template.content, true).firstElementChild]
-        :
-        selectors.map(selector => {
-            const node = document.importNode(template.content, true);
-            return node.querySelectorAll(selector);
-        }).reduce((components, nodes) => {
-            components.push(...nodes);
-            return components;
-        }, []);
-
-    const div = document.createElement("div");
-
-    div.dataset.template = Math.random().toString(32).slice(2);
-
-    components.forEach(component => div.appendChild(component));
-
-    return div;
-}
-
-function ubee(...components) {
-    // Process each component
-    components.forEach(element => {
-        ubee_event(element);
-        ubee_listen(element);
-
-        element.addEventListener(":update", () => {
-            ubee_attribute(element);
-            ubee_style(element);
-        });
-
-        fire(element, ":update");
-    });
-
-    return components;
 }
